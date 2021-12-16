377ffed36e1144261d5ffe74fc45889fca6c7d3c
mohammadmahdi.behrouzfar <mmbehroozfar@yahoo.com>
2021-12-16 23:03:23 +0330

    Add Domain Module

---
+++ domain/src/main/java/com/example/userslist/domain/baseusecases/UseCase.kt
@@ -0,0 +1,38 @@
+package com.example.userlist.domain.baseusecases
+
+import com.example.userlist.shared.resource.Resource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+/**
+ * Executes business logic synchronously or asynchronously using Coroutines.
+ */
+abstract class UseCase<R>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    /** Executes the use case asynchronously and returns a [Result].
+     *
+     * @return a [Result].
+     *
+     */
+    suspend operator fun invoke(): Resource<R> {
+        return try {
+            // Moving all use case's executions to the injected dispatcher
+            // In production code, this is usually the Default dispatcher (background thread)
+            // In tests, this becomes a TestCoroutineDispatcher
+            withContext(coroutineDispatcher) {
+                execute().let {
+                    Resource.Success(it)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(): R
+}
